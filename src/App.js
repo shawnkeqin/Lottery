ed40a0de29a8a92e5ad57b6f7fb3fca4ce0c612a
Shawn <e0052482@u.nus.edu>
2022-04-16 17:32:03 +0800

    Lottery app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useEffect, useState} from "react";
+import web3 from "./web3";
+import lottery from './lottery'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+
+const [data, setData] = useState({
+  manager: '',
+  players: [],
+  balance: '',
+  value: '',
+}); 
+
+const [message, setMessage] = useState(''); 
+
+useEffect(() => {
+
+  const callManager = async () => { 
+    await lottery.methods.manager().call(); 
+  }
+
+  const callPlayers = async () => {
+    await lottery.methods.getPlayers().call(); 
+  }
+
+  const getBalance = async () => { await web3.eth.getBalance(lottery.options.address); }
+
+  const mn = callManager(); 
+  const players = callPlayers();
+  const balance = getBalance(); 
+
+
+  setData({...data, mn, players, balance}); 
+},[data])
+
+const onSubmit = async (event) => {
+  event.preventDefault();
+
+  const accounts = await web3.eth.getAccounts();
+
+  setMessage('Waiting on transaction success...');
+
+  await lottery.methods.enter().send({
+    from: accounts[0],
+    value: web3.utils.toWei(data.value, 'ether')
+  })
+
+  setMessage('You have been entered!');
+
 }
 
+
+    return (
+      <div className="App">
+    <h2>Lottery Contract</h2>
+    <p>This contract is managed by {data.manager}
+    There are currently {data.players.length} people entered, competing to win {web3.utils.fromWei(data.balance, 'ether')} ether!</p>
+<hr />
+
+<form onSubmit={onSubmit}>
+  <h4>
+    Want to try your luck?
+  </h4>
+  <div>
+    <label>
+      <input onChange={event => setData({value: event.target.value}) } />
+      </label>
+  </div>
+  <button>Enter</button>
+</form>
+<hr />
+<h1>{message}</h1>
+      </div>
+    );
+}
 export default App;
